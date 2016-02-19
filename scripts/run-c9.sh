1bab3588be6705e03630152d8453a4d821d3658d
Robin <me@invokr.org>
2016-02-19 16:54:06 +0100

    Enable collab

--- scripts/run-c9.sh
+++ scripts/run-c9.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ~/.nvm/nvm.sh
 cd /c9sdk
-node server.js -p 8080 -l 0.0.0.0 -w /workspace -a {{ .Env.C9_USER }}:{{ .Env.C9_PASSWORD }}
+node server.js --collab -p 8080 -l 0.0.0.0 -w /workspace -a {{ .Env.C9_USER }}:{{ .Env.C9_PASSWORD }}
